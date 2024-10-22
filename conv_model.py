b8c96ec546975cf42de3e16c4da943b351bf9162
Elias-Salgado <105817789+Elias-Salgado@users.noreply.github.com>
2024-10-22 11:22:29 -0600

    Convolutional classifier files.

---
+++ conv_model.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 11:31:12 2024
+
+@author: Fuzzy Logic
+"""
+import torch
+import torchaudio
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+import data_managment
+# %% Data extraction
+training_generator = data_managment.training_generator
+test_generator = data_managment.test_generator
+
+# %% Model description
+network = nn.Sequential(
+            
+            nn.Conv2d(2, 32, kernel_size = 3, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(32,64, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+        
+            nn.Conv2d(64, 128, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(128 ,128, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            
+            nn.Conv2d(128, 256, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(256,256, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            
+            nn.Flatten(),
+            nn.Linear(1024,1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512,4))
+
+network.to("cuda:0")
+
+# %% Loss criterion
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(network.parameters(), lr=0.003, momentum=0.9)
+
+epochs = 10000
+batch = 40
+
+for epoch in range(epochs):
+    # Training
+    for local_batch, local_labels in training_generator:
+        # Transfer to GPU
+        local_batch, local_labels = local_batch.float().to("cuda:0"), local_labels.to("cuda:0")
+        # Optimization process
+        optimizer.zero_grad()
+        # forward + backward + optimize
+        outputs = network(local_batch)
+        loss = criterion(outputs, local_labels)
+        loss.backward()
+        optimizer.step()
+        loss_train = loss.item()
+        print("Train loss: {} Epoch:{}".format(loss_train,epoch))
+    if loss_train <= 0.01:
+        break
+
+len(training_generator)
+    
+# %% Accuracy computing
+# Train accuracy
+def compute_acc(generator):
+    acc = 0
+    lenght = 0
+    # Iterate over the dataset
+    for local_batch, local_labels in generator:
+        # Transfer to GPU
+        local_batch, local_labels = local_batch.float().to("cuda:0"), local_labels.to("cuda:0")
+        # Evaluate the model
+        pred = network(local_batch)
+        # Convert to numpy for statics
+        pred = pred.cpu().detach().numpy()
+        pred = np.argmax(pred, axis= 1)
+        y = local_labels.cpu().detach().numpy()
+        y = np.argmax(y, axis= 1)
+        acc = acc + np.count_nonzero(pred==y)
+        lenght = lenght + len(local_batch)
+    print(acc/lenght)
+
+    
+compute_acc(training_generator)
+
+compute_acc(test_generator)
---
+++ data_managment.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 11:21:00 2024
+
+@author: Fuzzy Logic
+"""
+import torch
+import torchaudio
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import pandas as pd
+import torchvision
+from sklearn.model_selection import train_test_split
+
+# %% Reading dataset
+# Handle dataset
+#data = pd.read_csv(r'C:\Users\Fuzzy Logic\Documents\Luis Elias\Bearing_Vibrations\0.014\0_HP\4C_0HP_014.csv', header = None)
+data_1 = pd.read_csv(r'C:\Users\Fuzzy Logic\Documents\Luis Elias\Bearing_Vibrations\0.007\general\4C_general_007.csv', header = None)
+data_2 = pd.read_csv(r'C:\Users\Fuzzy Logic\Documents\Luis Elias\Bearing_Vibrations\0.014\general\4C_general_014.csv', header = None)
+data_3 = pd.read_csv(r'C:\Users\Fuzzy Logic\Documents\Luis Elias\Bearing_Vibrations\0.021\general\4C_general_021.csv', header = None)
+
+data = pd.concat([data_1, data_2, data_3], ignore_index=True, axis=0)
+
+# %% Structural variables
+size = 16*16
+n_sample = int(len(data)/size)
+
+#%%  Normal dataset
+n_data = data.values[0:n_sample*size,[0,4]]
+n_data = np.reshape(n_data, (n_sample,2,16,16))
+n_label = np.repeat(0, n_sample)
+
+# Inner race fault dataset
+ir_data = data.values[0:n_sample*size,[1,5]]
+ir_data = np.reshape(ir_data, (n_sample,2,16,16))
+ir_label = np.repeat(1, n_sample)
+
+# Ball fault dataset
+b_data = data.values[0:n_sample*size,[2,6]]
+b_data = np.reshape(b_data, (n_sample,2,16,16))
+b_label = np.repeat(2, n_sample)
+
+# Outer race fault dataset
+or_data = data.values[0:n_sample*size,[3,7]]
+or_data = np.reshape(or_data, (n_sample,2,16,16))
+or_label = np.repeat(3, n_sample)
+
+# %% Organizing the dataset
+x_data = np.concatenate((n_data,ir_data,b_data,or_data))
+x_label = np.concatenate((n_label, ir_label, b_label, or_label), axis=0)
+# train and test split
+x_train, x_test, y_train, y_test = train_test_split(x_data, x_label, test_size=0.2)
+
+# %% Tesnor transformations
+x_train = torch.tensor(x_train).float()
+y_train = torch.tensor(y_train).long()
+
+x_test = torch.tensor(x_test).float()
+y_test = torch.tensor(y_test).long()
+# One hot codification
+y_train = F.one_hot(y_train, num_classes=4).float()
+y_test = F.one_hot(y_test, num_classes=4).float()
+
+# %% Dataloader class creation
+class Dataset(torch.utils.data.Dataset):
+  'Characterizes a dataset for PyTorch'
+  def __init__(self, data, labels):
+        'Initialization'
+        self.labels = labels
+        self.data = data
+
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.data)
+
+  def __getitem__(self, index):
+        'Generates one sample of data'
+        X = self.data[index]
+        y = self.labels[index]
+
+        return X, y
+    
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
+torch.backends.cudnn.benchmark = True
+
+# Parameters
+params = {'batch_size': 500,
+          'shuffle': True,
+          'num_workers': 0}
+
+# Generators
+training_set = Dataset(x_train, y_train)
+training_generator = torch.utils.data.DataLoader(training_set, **params)
+
+test_set = Dataset(x_test, y_test)
+test_generator = torch.utils.data.DataLoader(test_set, **params)
